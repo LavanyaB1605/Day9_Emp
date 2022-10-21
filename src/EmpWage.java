112e741307d82dac28bafa045b0d4a43fc870c25
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-10-22 00:13:17 +0530

    Uc4Emp

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -45,6 +45,24 @@ public class EmpWage {
             System.out.println("Employee Salary=" +salary);
         }
     }
+    public static void SwitchCase(){
+        switch (x) {
+            case 1:
+                empHrs=8;
+                salary=empRatePerHr * empHrs;
+                System.out.println("Employee PartTime Salary=" +salary);
+                break;
+            case 2:
+                empHrs=16;
+                salary=empRatePerHr * empHrs;
+                System.out.println("Employee FullTime Salary=" +salary);
+                break;
+            default:
+                empHrs=0;
+                salary=empRatePerHr * empHrs;
+                System.out.println("Employee Salary=" +salary);
+        }
+    }
 
 
 
--- src/MainEmp.java
+++ src/MainEmp.java
@@ -3,5 +3,6 @@ public class MainEmp extends EmpWage
     public static void main(String args[]){
         EmployeePresent();
         DailyWage();
+        PartTime();
     }
 }
