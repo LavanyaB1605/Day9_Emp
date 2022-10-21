7d74433001d2ba0870aaa3475e71a97536a41595
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-10-21 23:41:08 +0530

    Uc2Emp

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,6 +1,11 @@
 import java.util.Random;
 
 public class EmpWage {
+    public static int isPresent = 1;
+    public static int empRatePerHr = 20 ;
+    public static int empHrs = 8 ;
+    public static int salary;
+
     public static Random random = new Random();
     public static int x = random.nextInt(2);
     public static void EmployeePresent(){
@@ -11,5 +16,18 @@ public class EmpWage {
             System.out.println("Employee is absent");
         }
     }
+    public static void DailyWage(){
+
+        if (isPresent == x )
+        {
+            salary=empRatePerHr * empHrs ;
+            System.out.println("DailyWage is " +salary);
+        }
+        else{
+            salary = 0;
+            System.out.println("DailyWage is " +salary);
+        }
+    }
+
 
 }
--- src/MainEmp.java
+++ src/MainEmp.java
@@ -2,5 +2,6 @@ public class MainEmp extends EmpWage
 {
     public static void main(String args[]){
         EmployeePresent();
+        DailyWage();
     }
 }
